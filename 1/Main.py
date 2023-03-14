4a4236c7eeb01d1618ed0fade9d27d0b8bf50c08
szymqn <koodziejskisz@outlook.com>
2023-03-14 15:06:38 +0100

    lab1 done

---
+++ 1/Main.py
@@ -0,0 +1,30 @@
+def gauss(matrix, vector):
+    n = len(matrix)
+
+    for i in range(n):
+        max_row = i
+        for j in range(i+1, n):
+            if matrix[j][i] > matrix[max_row][i]:
+                max_row = j
+
+        for j in range(i+1, n):
+            factor = matrix[j][i] / matrix[i][i]
+            vector[j] -= factor * vector[i]
+
+            for k in range(i, n):
+                matrix[j][k] -= factor * matrix[i][k]
+
+    x = [0] * n
+    for i in range(n-1, -1, -1):
+        x[i] = round((vector[i] - sum(matrix[i][j] * x[j] for j in range(i+1, n))) / matrix[i][i])
+
+    return x
+
+
+m1 = [[-1, 2, 1],
+      [1, -3, -2],
+      [3, -1, -1]]
+
+v1 = [-1, -1, 4]
+
+print(gauss(m1, v1))
