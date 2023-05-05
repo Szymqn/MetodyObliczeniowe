86e340841b387d152b96585ba5b447ca1922f637
szymqn <koodziejskisz@outlook.com>
2023-05-05 18:47:18 +0200

    update trapaze_method

--- 7/main.py
+++ 7/main.py
@@ -2,31 +2,36 @@ from math import sqrt
 
 
 def integral(x):
-    return (sqrt(x ** 2 + .3))/(1.4 + sqrt(.8 * (x ** 2) + 1.6))
+    # return (sqrt(x ** 2 + .3))/(1.4 + sqrt(.8 * (x ** 2) + 1.6))
+    return x ** 2
 
 
 def trapeze_method(integral, lower_limit, upper_limit, n):
     result = 0
     results = []
-    h = (upper_limit - lower_limit) / n
+    lower_limit *= 10
+    upper_limit *= 10
+    h = round((upper_limit - lower_limit) / n, 1)
 
-    for i in range(1, upper_limit+1):
-        results.append(integral(i))
+    for i in range(int(lower_limit), int(upper_limit)+int(h), int(h)):
+        results.append(integral(i/10))
 
-    print(results)
-
-    for i in range(upper_limit):
+    for i in range(len(results)):
         if i == 0 or i == len(results)-1:
             result += results[i] / 2
         else:
             result += results[i]
 
-    return h * result
+    return h * result / 10
 
 
 if __name__ == '__main__':
-    lower_limit = 1.3
-    upper_limit = 2.5
+    lower_limit = 1
+    upper_limit = 4
     n = 3
+
+    # lower_limit = 1.3
+    # upper_limit = 2.5
+    # n = 3
 
     print(f"Wynik dla n = {n}:", trapeze_method(integral, lower_limit, upper_limit, n))
