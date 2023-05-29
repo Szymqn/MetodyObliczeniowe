fcaafb7a09d4e42b55d69e1ba51ab2cafad1c230
szymqn <koodziejskisz@outlook.com>
2023-05-29 16:46:52 +0200

    move func to module

--- 10/main.py
+++ 10/main.py
@@ -1,34 +1,6 @@
-import math
 import numpy as np
-from scipy.integrate import quad
-
-
-def gauss_quadrature(n):
-    x, w = np.polynomial.legendre.leggauss(n)
-    return x, w
-
-
-def gauss_legendre(f, lower_limit, upper_limit, n):
-    result = 0
-    quadrature = gauss_quadrature(n)
-    abscissa = quadrature[0]
-    weight = quadrature[1]
-
-    h = (upper_limit - lower_limit) / 2
-
-    for i in range(n):
-        result += weight[i] * f(h * abscissa[i] + (upper_limit + lower_limit) / 2)
-
-    return h * result
-
-
-def legendre_poly(n, x):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return x
-    else:
-        return ((2 * n - 1) * x * legendre_poly(n - 1, x) - (n - 1) * legendre_poly(n - 2, x)) / n
+import math
+from funcs import gauss_quadrature, gauss_legendre, legendre_poly
 
 
 def approx_legendre(f, n, a, b, x):
